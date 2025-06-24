9d0e455a69b9612fb8c35d3dfe52b192ed1a9fe3
Sruthirangaraj03 <sruthir.22cse@kongu.edu>
2025-06-24 09:25:27 +0530

    Time: 1 ms (69.79%), Space: 42.1 MB (89.2%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.java
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.java
@@ -1,9 +1,13 @@
 class Solution {
     public String gcdOfStrings(String str1, String str2) {
+    StringBuilder a = new StringBuilder();
     int slice=0;
     if ((str1+str2).equals(str2+str1)){
         slice=compute(str1.length(),str2.length());
-        return str1.substring(0,slice);
+        for(int i=0;i<slice;i++){
+           a.append(str1.charAt(i));
+        }
+        return a.toString();
     }
      return "";
     }
